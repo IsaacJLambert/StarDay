5a1c0885d16e8fc615fc6dbff5a0d6ba0dbd2892
IsaacJLambert <eyeslick@gmail.com>
2022-02-20 19:18:56 -0700

    Has API error handleing and video

--- script.js
+++ script.js
@@ -5,19 +5,34 @@ document.getElementById("dateSubmit").addEventListener("click", function(event) 
     const value = document.getElementById("dateInput").value;
     if (value === "")
       return;
-    console.log(value);
     const url = `https://api.nasa.gov/planetary/apod?api_key=kUZxRExxvi15Q8T3ydciSOTQNu6ZOKg8Pzbbyx2z&date=${value}`;
     fetch(url)
       .then(function(response) {
         return response.json();
       }).then(function(json) {
         let returnedStarInfo = "";
-        console.log(json);
-        returnedStarInfo += `<h2>${value}</h2>`;
-        returnedStarInfo += `<img src="${json.url}">`;
-        returnedStarInfo += `<h2>${json.title}</h2><br>`;
-        returnedStarInfo += `<h4>Copyright: ${json.copyright}</h4><br>`;
-        returnedStarInfo += `<p>${json.explanation}</p>`;
+        if(json.code === 400) {
+          returnedStarInfo += `<h2>${value} is an invalid input.</h2><br><h2> Please verify the input is YYYY-MM-DD</h2><br><h2>Make sure the date is after 1995-06-16</h2>`;
+        }
+        else if(json.media_type === "video") {
+          returnedStarInfo += `<h2>${value}</h2>`;
+          returnedStarInfo += `<div id="video"><iframe src="${json.url}" allow="fullscreen"></iframe></div>`
+          returnedStarInfo += `<h2>${json.title}</h2><br>`;
+          if (json.copyright !== undefined) {
+            returnedStarInfo += `<h4>Copyright: ${json.copyright}</h4><br>`;
+          }
+          returnedStarInfo += `<p>${json.explanation}</p>`;
+        }
+        else {        
+          console.log(json);
+          returnedStarInfo += `<h2>${value}</h2>`;
+          returnedStarInfo += `<img src="${json.url}">`;
+          returnedStarInfo += `<h2>${json.title}</h2><br>`;
+          if (json.copyright !== undefined) {
+            returnedStarInfo += `<h4>Copyright: ${json.copyright}</h4><br>`;
+          }
+          returnedStarInfo += `<p>${json.explanation}</p>`;
+        }
         document.getElementById("dateResults").innerHTML = returnedStarInfo;
       });
   });
